3d38452b893315368a911b9dc407b78ab439c0b9
codes <codesver@gmail.com>
2025-07-31 20:11:27 +0900

    init

---
+++ src/main/java/coztion/springai/CoztionSpringAiApplication.java
@@ -0,0 +1,13 @@
+package coztion.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoztionSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoztionSpringAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coztion/springai/CoztionSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package coztion.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoztionSpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
